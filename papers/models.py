ce06276cc297f21552ceda95c0674cbde2b1d946
Godwin James <godwinjames670@gmail.com>
2024-08-12 05:41:32 +0100

    fixed the file path naming

--- papers/models.py
+++ papers/models.py
@@ -7,6 +7,17 @@ from useraccount.models import User
 
 # Create your models here.
 
+import os
+
+def get_paper_cover_path(instance, filename):
+    # Return the path to save the cover page
+    return os.path.join('uploads', 'papers', instance.name, 'cover', filename)
+
+def get_paper_file_path(instance, filename):
+    # Return the path to save the paper file
+    return os.path.join('uploads', 'papers', instance.name, filename)
+
+
 class Type(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField( max_length=200, unique=True)
@@ -35,30 +46,27 @@ class SubCategory(models.Model):
 
 
 class Papers(models.Model):
-    id = models.UUIDField( default=uuid.uuid4, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False)
     name = models.CharField(primary_key=True, max_length=200)
     author = models.CharField(max_length=255)
-    abstract =  models.TextField(null=True, blank=True)
-    cover_page = models.ImageField(upload_to=f"uploads/papers/{name}/cover", null=True, blank=True)
+    abstract = models.TextField(null=True, blank=True)
+    cover_page = models.ImageField(upload_to=get_paper_cover_path, null=True, blank=True)
     type = models.ForeignKey(Type, related_name='type', on_delete=models.SET_NULL, null=True, blank=True)
     category = models.ForeignKey(Category, related_name='category', on_delete=models.SET_NULL, null=True, blank=True)
     subcategory = models.ForeignKey(SubCategory, related_name='subcategory', on_delete=models.SET_NULL, null=True, blank=True)
     year_published = models.DateField(null=True, blank=True)
-    date_uploaded = models.DateTimeField(auto_now_add=True,)
-    file = models.FileField(upload_to=f"uploads/papers/{name}", null=True, blank=True)
-    is_open_access = models.BooleanField(null=True, blank=True) 
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to=get_paper_file_path, null=True, blank=True)
+    is_open_access = models.BooleanField(null=True, blank=True)
     is_approved = models.BooleanField(null=True, blank=True)
     citations = models.IntegerField(null=True, blank=True)
     references = models.TextField(null=True, blank=True)
     comments = models.TextField(null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
     resource_id = models.CharField(max_length=255, null=True, blank=True)
-    # file = models.FileField()
-
 
     def cover_url(self):
-        return f"{settings.WEBSITE_URL}{self.image.url}"
+        return f"{settings.WEBSITE_URL}{self.cover_page.url}"
     
     def __str__(self):
         return self.name
-    
