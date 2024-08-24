262a87d024aad0c35d950d1e75b0f15dde7e45c7
Godwin James <godwinjames670@gmail.com>
2024-08-25 00:01:34 +0100

    reconfigured ProfileSerializer

--- useraccount/serializers.py
+++ useraccount/serializers.py
@@ -50,7 +50,7 @@ class CustomRegisterSerializer(RegisterSerializer):
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['__all__']
+        fields = ['name', 'last_name', 'email', 'avatar', 'education', 'institution', 'is_staff', 'is_publisher',]
 
 
 import logging
